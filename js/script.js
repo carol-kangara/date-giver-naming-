38fd6a1d976abbade50857ce3d5c380ae5e9d2de
carol-kangara <carolkangara30@gmail.com>
2020-12-14 19:24:40 +0300

    new h1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,65 @@
-va
+var date=document.getElementById("date").value;
+var month=document.getElementById("month").value;
+var year=document.getElementById("year").value
+function birthDay(){
+
+   var CC = math.trunc(parseInt((year/100)));
+    var YY = parseInt(year.substring(2,4));
+    var MM = parseInt(document.getElementById("month").value);
+    var DD = parseInt(document.getElementById("date").value);
+    dayValue = ( ( (CC/4) -2*CC-1) + ( (5*YY/4) ) + ((26*(MM+1)/10) ) + DD)%7;
+    console.log(dayValue);
+    return (Math.trunc(birthDay(dayValue)));
+}
+function gender(){
+    var gender=["female","male"]
+    var female=document.getElementById("female").value;
+    var male=document.getElementById("male").value;
+    console.log(gender());
+if(birthDay==0 && gender==female){
+    alert("You were born on sunday and Your akan name is Akosua");
+}
+else if(birthDay==1 && gender==female){
+    alert("You were born on monday and Your akan name is Adwoa");
+}
+else if(birthDay==2 && gender==female){
+    alert("You were born on Teusday and Your akan name is Abenaa");
+}
+else if(birthDay==3 && gender==female){
+    alert("You were born on Wednesday and Your akan name is Akua");
+}
+else if(birthDay==4 && gender==female){
+    alert("You were born on Thursday and Your akan name is Yaa");
+}
+else if(birthDay==5 && gender==female){
+    alert("You were born on Friday and Your akan name is Afua");
+}
+else if(birthDay==6 && gender==female){
+    alert("You were born on Saturday and Your akan name is Ama");
+}
+if(birthDay==0 && gender==male){
+    alert("You were born on sunday and Your akan name is kwasi");
+}
+else if(birthDay==1 && gender==male){
+    alert("You were born on monday and Your akan name is kwadwo");
+}
+else if(birthDay==2 && gender==male){
+    alert("You were born on Teusday and Your akan name is kwabena");
+}
+else if(birthDay==3 && gender==male){
+    alert("You were born on Wednesday and Your akan name is kwaku");
+}
+else if(birthDay==4 && gender==male){
+    alert("You were born on Thursday and Your akan name is yaw");
+}
+else if(birthDay==5 && gender==male){
+    alert("You were born on Friday and Your akan name is kofi");
+}
+else if(birthDay==6 && gender==male){
+    alert("You were born on Saturday and Your akan name is kwame");
+}
+    
+};
+
+dayValue = birthDay();
+gender();
